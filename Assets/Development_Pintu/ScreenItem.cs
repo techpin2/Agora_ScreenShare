ca1750ebc8ff7593a6b240a45a3d100544f02028
Pintu <pintupk18122000@gmail.com>
2024-07-05 15:56:15 +0530

    #Updated Outline On Screen Item

--- Assets/Development_Pintu/ScreenItem.cs
+++ Assets/Development_Pintu/ScreenItem.cs
@@ -1,19 +1,23 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScreenItem : MonoBehaviour
 {
-    [SerializeField] private RawImage screenImage; 
+    [SerializeField] private RawImage screenImage;
     [SerializeField] private TMP_Text windowName;
+    [SerializeField] private GameObject outer;
     private Button button;
     private string windowId;
 
+    private static ScreenItem currentlySelectedScreenItem;
+
     private void Awake()
     {
-        button= GetComponent<Button>();
+        button = GetComponent<Button>();
         button.onClick.AddListener(OnClickScreenItem);
     }
+
     private void OnDestroy()
     {
         button.onClick.RemoveListener(OnClickScreenItem);
@@ -21,18 +25,44 @@ public class ScreenItem : MonoBehaviour
 
     public virtual void OnClickScreenItem()
     {
-        ScreenShare_Child screnShare_Child= FindAnyObjectByType<ScreenShare_Child>();
-        screnShare_Child.OnStartShareBtnClicked(windowId);
+        // Deselect the previously selected ScreenItem if it exists
+        if (currentlySelectedScreenItem != null && currentlySelectedScreenItem != this)
+        {
+            currentlySelectedScreenItem.Deselect();
+        }
+
+        // Select this ScreenItem
+        Select();
+
+        // Call the ScreenShare_Child method
+        ScreenShare_Child screenShare_Child = FindAnyObjectByType<ScreenShare_Child>();
+        screenShare_Child.OnStartShareBtnClicked(windowId);
     }
 
     public void UpdateScreenItemTitle(string windowName)
     {
-        string[] name = windowName.Split('|');
-        this.windowName.text = name[0];
-        this.windowId= name[name.Length-1];
+        string[] screenName = windowName.Split('|');
+        this.windowName.text = screenName[0];
+        this.windowId = screenName[screenName.Length - 1];
     }
+
     public void UpdateScreenItemThumbnail(Texture texture)
     {
         this.screenImage.texture = texture;
     }
-} 
+
+    private void Select()
+    {
+        outer.SetActive(true);
+        currentlySelectedScreenItem = this;
+    }
+
+    private void Deselect()
+    {
+        outer.SetActive(false);
+        if (currentlySelectedScreenItem == this)
+        {
+            currentlySelectedScreenItem = null;
+        }
+    }
+}
